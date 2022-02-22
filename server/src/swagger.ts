400b49d52ed6238307888af5b7cd20c57337f8cd
Ofek Gabay <61761153+tupe12334@users.noreply.github.com>
2022-02-22 18:30:16 +0200

    amplication-build-ckzycfr5w73093901s6gosyuwgu

--- server/src/swagger.ts
+++ server/src/swagger.ts
@@ -7,7 +7,7 @@ export const swaggerDocumentOptions = new DocumentBuilder()
   .setDescription(
     'Sample application for e-commerce\n\n## Congratulations! Your application is ready.\n  \nPlease note that all endpoints are secured with HTTP Basic authentication.\nBy default, your app comes with one user with the username "admin" and password "admin".\nLearn more in [our docs](https://docs.amplication.com)'
   )
-  .setVersion("1h95i3cu")
+  .setVersion("glt8oh8e")
   .addBasicAuth()
   .build();
 
